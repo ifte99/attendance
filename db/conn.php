3edd5502462191f14eb1dfc5e4bbd6a898752cfd
ifte99 <87614310+ifte99@users.noreply.github.com>
2021-08-13 15:17:14 +0600

    added remote mysql connection details

--- db/conn.php
+++ db/conn.php
@@ -1,9 +1,17 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+   //Development Connection
+   // $host = '127.0.0.1';
+    //$db = 'attendance_db';
+    // $user = 'root';
+    //$pass = '';
+    // $charset= 'utf8mb4';
+  // remote database connection
+    $host = 'remotemysql.com';
+    $db = '0ONwCsVEZA';
+    $user = '0ONwCsVEZA';
+    $pass = 'nnCxhgonMR';
     $charset= 'utf8mb4';
+
 
     $dsn= "mysql:host=$host;dbname=$db;charset=$charset";
 
